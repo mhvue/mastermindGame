ae84554bbd7da6f027ecf2f99c690ee2d8f84483
mhvue <mhvue4@gmail.com>
2022-05-18 00:55:02 -0500

    added in keydown to get value from keys pressed. console log to check. added in form to enter number.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
-$("h1").click(function() {
-    console.log("clicked")
+let enteredNum = []
+
+//clear form when clicked on 
+$("input").click(function() {
+    $("input").val(" ");
 })
+
+//get the number from user and and to enteredNumArr
+$(".numHolder").keydown(function(event){
+    enteredNum.push(event.key) //numbers entered are strings
+});
+
