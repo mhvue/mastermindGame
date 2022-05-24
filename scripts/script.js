304c18dd4323da65fd2930068dc540dfe882c745
mhvue <mhvue4@gmail.com>
2022-05-24 01:08:31 -0500

    added in regex to check for input number validation. fixed modal class so correct messages are showing. stop page from refreshing when pressing enter.

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,11 @@ let enteredNum = [];
 let newNum = [];
 let count = 10;
 let score = 0;
-//api here. get the generated numbers and put into generatedNum array.
+
+/**
+ * 
+ * @returns generated numbers from api
+ */
 function randomNum () {
    
     return $.ajax({
@@ -27,7 +31,11 @@ function randomNum () {
 randomNum();
 
 
-//modal messages 
+/**
+ * function to create custom messages as modal pops up
+ * @param {string} message 
+ * @param {string} image 
+ */
 function messageModal (message, image){
     image = image || "";
 
@@ -36,21 +44,25 @@ function messageModal (message, image){
     $(".try-title").text("Try Again");
     $(".gameOver-title").text("Game Over");
     $(".almost-title").text("Almost");
-   $(".correct-title").text("Congrats!");
-   $(".hint-title").text("Hint");
+    $(".correct-title").text("Congrats!");
+    $(".hint-title").text("Hint");
     $(".messageContainer").html(message);
     $(image).prependTo(".messageContainer");
 }
 
-
+/**
+ * function as the count goes down
+ * when count reaches 0, game is over. modals pops up informing of so.
+ * eveyrone resets (score, feedback gone, count)
+ *  button to play again shows up.
+ */
 function countDown() {
     count--;
-    //console.log("count is now " + count)
     $("#guess-num").text(count);
 
     //halfway count
     if(count === 5) {
-        $("#guess-num").css("color", "#8B0000");
+        $("#guess-num").css("color", "#FF0000");
     }
 
     //game over
@@ -85,7 +97,8 @@ $("#reset-btn").on("click", function(event) {
 });
 
 //play again button on modal 
-$(".playAgainBtn").on("click", function() {
+$(".playAgainBtn").on("click", function(event) {
+    event.preventDefault();
     randomNum();
     count = 10;
     $("#submitBtn").prop("disabled", false);
@@ -102,6 +115,7 @@ $("#submitBtn").click(function(event){
     let incorrectArr = []; 
     let containsArr =[];
     let splitUserNum;
+    const checkRegEx = /^[0-7]+$/; //with the help from stackOverflow
    
     //get the number from user and push into enteredNumArr 
     const userNum= $(".numHolder").val().trim();
@@ -111,41 +125,41 @@ $("#submitBtn").click(function(event){
     console.log(userNum.length)
 
     //Checking Values-make sure values are no more than 4 positions but no less. 
-    if(userNum.length > 4 || userNum.length <= 3) { 
+    if(userNum.match(checkRegEx) && userNum.length === 4){
+        enteredNum.push(userNum);
+        splitUserNum = enteredNum.join().split("");
+    }
+    else if(userNum.length > 4 || userNum.length <= 3) { 
         console.log("shortChar")
         $(".modal-title").removeClass("intro-title almost-title gameOver-title correct-title hint-title").addClass("try-title");
         return messageModal("Please input 4 numbers.");
    }
-   //make values are not bigger than entering 7 four times
-   else if(userNum > "7777" || userNum.includes("8") || userNum.includes("9") ){
-       return messageModal("Numbers bigger than 7 detected. Please input 0-7.");
-   }
-   else {
-        enteredNum.push(userNum);
-        splitUserNum = enteredNum.join().split("");
-    }
    
-    console.log(splitUserNum)
+    else{
+        //make values are not bigger than entering 7 four times
+        console.log("false")
+        return messageModal("Numbers bigger than 7 detected. Please input 0-7.");
+    }
+
+  
+
+ 
   //compared enteredNum values to genreatedNum values 
     for(let i = 0; i < splitUserNum.length; i++){        
        //guest correct number and its location
        if(splitUserNum[i] === newNum[i]){
            //check value and check index of each array
           checker.push(splitUserNum[i])
-        //    console.log("checker", checker, checker[i], i)
-        //     console.log(enteredNum)
-        //     console.log(splitUserNum[i])
+
           
         }
         //if user input the num already verified as correct, but in combo with differ nums
         else if(splitUserNum.includes(newNum[i])){
-            console.log(containsArr)
             containsArr.push(newNum[i])
         }
         else {
             //push the non matched numbers 
             incorrectArr.push(splitUserNum[i])
-          //  console.log("incorr ", incorrectArr)
          
             if(i === enteredNum.length-1) {
                 enteredNum.length = 0;
@@ -156,11 +170,11 @@ $("#submitBtn").click(function(event){
 //messages to user
     if(checker.length === 4) {
         const correctImg = $("<img src='./images/correctBtn.svg' alt= 'red box with white x' >");
-        const correctMsg = $("<h5> You got all the numbers corret. Play Again?</h5>").addClass("text-position")
+        const correctMsg = $("<h5> You got all the numbers corret. Play Again?</h5>");
 
         score++;
         $(".scoreBoard").text(score);
-        $(".modal-title").addClass("correct-title");
+        $(".modal-title").removeClass("intro-title almost-title gameOver-title hint-title try-title").addClass("correct-title");
         messageModal(correctImg, correctMsg);
         $(".playAgainBtn").show();
         $("#reset-btn").text("Play Again").addClass(".playAgainBtn");
@@ -201,6 +215,8 @@ $("#submitBtn").click(function(event){
 
         //look at checker length to provide some kind of hint 
         if(checker.length === 2 || containsArr.length === 2){
+            $(".modal-title").removeClass("intro-title try-title gameOver-title correct-title hint-title")
+                          .addClass("almost-title");
             $(".messageContainer").text("Pssstt..You got two numbers correct. Keep going!");
         }
 
@@ -227,7 +243,7 @@ $("#hint-btn").click(function() {
    const hintMsg = $("<p>").text(`It contains ${newNum[randomIndex]}`).addClass("text-position");
 
    //show modal with hint
-   $(".modal-title").removeClass().addClass("hint-title");
+   $(".modal-title").removeClass("intro-title almost-title gameOver-title  correct-title try-title").addClass("hint-title");
    messageModal(hintImg, hintMsg);
    $(".playAgainBtn").hide();
    $("#hint-btn").prop("disabled", true);
